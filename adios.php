d339744c75e8fa0c8731ef1849aee8fe974a1f98
Alejandra6891 <aljalvarezro@gmail.com>
2021-10-29 23:14:50 +0200

    Creado proyecto inicial

---
+++ adios.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+    <meta charset="utf-8">
+      <title>Segundo PHP</title>
+  </head>
+  <body>
+      <?php
+        echo "adios desarrolador!!!!";
+       ?>
+  </body>
+</html>
